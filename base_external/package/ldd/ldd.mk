adc28687ff3f2168c345129b0e320c6c0938c3e1
jblob <juergen.blob@cariad.technology>
2026-05-07 21:14:12 +0200

    assignemnt7.2-3 - commit2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,16 @@
+LDD_VERSION = 1.0
+LDD_SITE = $(TOPDIR)
+LDD_SITE_METHOD = local
+
+define LDD_BUILD_CMDS
+    $(MAKE) -C $(TOPDIR)/../misc-modules KDIR=$(LINUX_DIR)
+    $(MAKE) -C $(TOPDIR)/../scull KDIR=$(LINUX_DIR)
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)
+    cp $(TOPDIR)/../misc-modules/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/
+    cp $(TOPDIR)/../scull/*.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/
+endef
+
+$(eval $(generic-package))
