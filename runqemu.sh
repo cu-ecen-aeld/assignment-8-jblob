5be14cb4e2c804ee63dcbf59cb6aa8a7bb34c2b7
jblob <juergen.blob@cariad.technology>
2026-05-09 22:10:30 +0200

    fixed yaml(6)

--- runqemu.sh
+++ runqemu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script to start qemu
 
-#source poky/oe-init-build-env
-#export QB_SLIRP_OPT="-netdev user,id=net0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000"
-#runqemu slirp nographic
+source poky/oe-init-build-env
+export QB_SLIRP_OPT="-netdev user,id=net0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000"
+runqemu slirp nographic
 
 #qemu-system-i386 \
   -kernel buildroot/output/images/bzImage \
@@ -12,8 +12,18 @@
   -netdev user,id=net0,hostfwd=tcp::10022-:22 \
   -device e1000,netdev=net0 \
   -nographic
+  
+#source poky/oe-init-build-env
 
-qemu-system-i386 \
+#runqemu \
+  -kernel buildroot/output/images/bzImage \
+  -drive file=buildroot/output/images/rootfs.ext2,format=raw \
+  -append "root=/dev/sda console=ttyS0 noapic ip=dhcp" \
+  -netdev user,id=net0,hostfwd=tcp::10022-:22 \
+  -device rtl8139,netdev=net0 \
+  -nographic
+
+#qemu-system-i386 \
   -kernel buildroot/output/images/bzImage \
   -drive file=buildroot/output/images/rootfs.ext2,format=raw \
   -append "root=/dev/sda console=ttyS0 noapic ip=dhcp" \
