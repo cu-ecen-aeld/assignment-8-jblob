b7974d1b27a8b1ddef94971f7ba82adfdecc99e2
jblob <juergen.blob@cariad.technology>
2026-05-09 23:02:13 +0200

    fixed yaml und qemu run script

--- runqemu.sh
+++ runqemu.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
-# Script to start qemu
 
+# Start Yocto QEMU correctly
 source poky/oe-init-build-env
-export QB_SLIRP_OPT="-netdev user,id=net0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000"
-runqemu slirp nographic
-
-#qemu-system-i386 \
-  -kernel buildroot/output/images/bzImage \
-  -drive file=buildroot/output/images/rootfs.ext2,format=raw \
-  -append "root=/dev/sda console=ttyS0 noapic ip=dhcp" \
-  -netdev user,id=net0,hostfwd=tcp::10022-:22 \
-  -device e1000,netdev=net0 \
-  -nographic
-  
-#source poky/oe-init-build-env
-
-#runqemu \
-  -kernel buildroot/output/images/bzImage \
-  -drive file=buildroot/output/images/rootfs.ext2,format=raw \
-  -append "root=/dev/sda console=ttyS0 noapic ip=dhcp" \
-  -netdev user,id=net0,hostfwd=tcp::10022-:22 \
-  -device rtl8139,netdev=net0 \
-  -nographic
-
-#qemu-system-i386 \
-  -kernel buildroot/output/images/bzImage \
-  -drive file=buildroot/output/images/rootfs.ext2,format=raw \
-  -append "root=/dev/sda console=ttyS0 noapic ip=dhcp" \
-  -netdev user,id=net0,hostfwd=tcp::10022-:22 \
-  -device rtl8139,netdev=net0 \
-  -nographic
+runqemu qemux86-64 nographic slirp
