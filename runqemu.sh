1cd7d304c66df66034c00e0e386076868cd5406b
jblob <juergen.blob@cariad.technology>
2026-05-12 14:51:07 +0200

    fixed yaml and qemu start script

--- runqemu.sh
+++ runqemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Umgebung im vorhandenen build-Ordner laden
+# Umgebung laden
 source poky/oe-init-build-env build
 
-# Startet QEMU mit User-Networking. 
-# QB_SLIRP_OPT aus der local.conf übernimmt das Port-Forwarding (10022).
+# QEMU starten. Er verwendet automatisch das Image aus dem persistenten /build/tmp Ordner.
+# Das slirp-Argument sorgt dafür, dass unser Port-Forwarding aus der local.conf aktiv wird.
 runqemu nographic slirp
