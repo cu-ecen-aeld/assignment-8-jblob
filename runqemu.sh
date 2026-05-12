b162b031a02d74e51c66d14c67d7131e313bf35f
jblob <juergen.blob@cariad.technology>
2026-05-12 15:17:07 +0200

    fixed yaml and qemu start script

--- runqemu.sh
+++ runqemu.sh
@@ -2,10 +2,10 @@
 # Umgebung laden
 source poky/oe-init-build-env build
 
-# Wir löschen QEMU_EXTRA_ARGS, falls sie von außen kommen, 
-# um Konflikte mit der local.conf zu vermeiden
+# Wir löschen Extra-Args, um Dopplungen zu vermeiden
 export QEMU_EXTRA_ARGS=""
 
-# Starte QEMU mit nographic.
-# Das 'slirp' Kommando triggert die QB_SLIRP_OPT aus der local.conf korrekt an.
+# Starte QEMU. 
+# Da QB_SLIRP_OPT in der local.conf jetzt das komplette '-netdev' enthält,
+# wird runqemu dies korrekt in die Befehlszeile einbauen.
 runqemu nographic slirp
