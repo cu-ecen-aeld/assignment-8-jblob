03f272519b0f1444ed197e7b7f896edcdf2f8a0f
jblob <juergen.blob@cariad.technology>
2026-05-12 14:39:36 +0200

    fixed yaml and qemu start script

--- runqemu.sh
+++ runqemu.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Nutze die Bash explizit
-# Pfade relativ zum Projekt-Root setzen
+# Umgebung im vorhandenen build-Ordner laden
 source poky/oe-init-build-env build
 
-# Wir verlassen uns auf die QB_SLIRP_OPT in der local.conf
-# Da runqemu.sh oft im Hintergrund gestartet wird, stellen wir sicher, 
-# dass es das richtige Verzeichnis nutzt.
+# Startet QEMU mit User-Networking. 
+# QB_SLIRP_OPT aus der local.conf übernimmt das Port-Forwarding (10022).
 runqemu nographic slirp
